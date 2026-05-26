e7b1f07c8894840dfdc1d9aff081a45b936cc8eb
Savinov Arseny <savinov.av@intechs.by>
2026-05-26 18:49:39 -0400

    managre renew

--- manager/manager.go
+++ manager/manager.go
@@ -20,12 +20,14 @@ import (
 
 type Manager struct {
 	Pending      queue.Queue
-	TaskDManage  map[string][]*task.Task
-	EventDManage map[string][]*task.TaskEvent
+	TaskDManage  map[uuid.UUID]*task.Task
+	EventDManage map[uuid.UUID]*task.TaskEvent
 	Workers      []string
 
 	WorkerTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
+
+	LastWorker int
 
 	WorkerNodes []*node.Node
 	Scheduler   scheduler.Scheduler
@@ -39,16 +41,110 @@ func NewManager(workers []string, schedulerType string) *Manager {
 	}
 }
 
-func (m *Manager) SelectWorker() error {
-	return errors.New("not implement")
+func (m *Manager) SelectWorker() string {
+	var newWorker int
+	if m.LastWorker+1 < len(m.Workers) {
+		newWorker = m.LastWorker + 1
+		m.LastWorker++
+	} else {
+		newWorker = 0
+		m.LastWorker = 0
+	}
+
+	return m.Workers[newWorker]
 }
 
-func (m *Manager) UpdateTasks() error {
-	return errors.New("not implement")
+func (m *Manager) UpdateTasks()  {
+	for _, worker := range m.Workers{
+		log.Printf("Checkimg worker %v for task updates", worker.)
+		url := fmt.Sprintf("http://%s/tasks", worker)
+		resp, err := http.Get(url)
+		if err != nil{
+			log.Printf("Error connecton to %v: %v", worker, err)
+		}
+
+
+		if resp.StatusCode != http.StatusOK{
+			log.Printf("Error sending request to %v: %v", worker, resp.Status)
+		}
+
+		d := json.NewDecoder(resp.Body)
+		var tasks []*task.Task
+		err = d.Decode(&tasks)
+		if err != nil{
+			log.Printf("Error decoding response from %v: %v", worker, err)
+		}
+
+		for _, t := range tasks{
+			log.Printf("Attemp to update task %v", t)
+
+
+			_,ok := m.TaskDManage[t.ID]
+			if !ok{
+				log.Printf("Task with ID %s not fount", t.ID)
+				return
+			}
+
+
+			if m.TaskDManage[t.ID].State != t.State{
+				m.TaskDManage[t.ID].State = t.State
+			}
+
+
+			m.TaskDManage[t.ID].StartTime = t.StartTime
+			m.TaskDManage[t.ID].EndTime = t.EndTime
+			m.TaskDManage[t.ID].ContainerId = t.ContainerId
+			}
+	}
 }
 
-func (m *Manager) SendWork() error {
-	return errors.New("not implement")
+func (m *Manager) SendWork() {
+	if m.Pending.Len() > 0 {
+		w := m.SelectWorker()
+
+		e := m.Pending.Dequeue()
+		te := e.(task.TaskEvent)
+		t := te.Task
+		log.Printf("Pulled %v off pending queue\n", t)
+
+		m.EventDManage[te.ID] = &te
+		m.WorkerTaskMap[w] = append(m.WorkerTaskMap[w], te.Task.ID)
+		m.TaskWorkerMap[t.ID] = w
+
+		t.State = task.Scheduled
+		m.TaskDManage[t.ID] = &t
+
+		data, err := json.Marshal(te)
+		if err != nil {
+			log.Printf("unable to marshal task event: %v", err)
+		}
+
+		url := fmt.Sprintf("htpp://%s/task", w)
+
+		resp, err := http.Post(url, "application/json", bytes.NewBuffer(data))
+		if err != nil {
+			log.Printf("Error connecting to %v: %v\n", w, err)
+			m.Pending.Enqueue(te)
+			return
+		}
+
+		d := json.NewDecoder(resp.Body)
+		if resp.StatusCode != http.StatusOK {
+			e := worker.ErrResponse{}
+			err := d.Decode(&e)
+			if err != nil {
+				fmt.Printf("Error decoding response: %v\n", err)
+				return
+			}
+			log.Printf("Response error: %v\n", e)
+			return
+		}
+		log.Printf("Response status: %v\n", resp.Status)
+
+	} else {
+		log.Printf("No work in the queue")
+	}
+
 }
 
 func (m *Manager) checkTaskHealth(t task.Task) error {
